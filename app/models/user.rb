38c9b5c495b05b3311f4b5a140c230b15c2588af
sergiomarrocoli <s.marrocoli@gmail.com>
2023-08-13 16:32:19 +0100

    Refactor authentication code in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Authentication
+
   validates :name, presence: true
   validates :email,
     format: { with: URI::MailTo::EMAIL_REGEXP },
@@ -7,26 +9,7 @@ class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
   
-  has_secure_password
-  validates :password,
-  presence: true,
-  length: { minimum: 8 }
-  
-  has_many :app_sessions
-
   before_validation :strip_extraneous_spaces
-
-  def self.create_app_session(email:, password:)
-    return nil unless user = User.find_by(email: email.downcase)
-
-    user.app_sessions.create if user.authenticate(password)
-  end
-
-  def authenticate_app_session(app_session_id, token)
-    app_sessions.find(app_session_id).authenticate_token(token)
-  rescue ActiveRecord::RecordNotFound
-    nil
-  end
 
   private
 
