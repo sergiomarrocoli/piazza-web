1da3f9a57c3b06b45424171f944706e6941cdb1b
sergiomarrocoli <s.marrocoli@gmail.com>
2023-08-14 20:32:48 +0100

    Add system tests for login and sign up

---
+++ test/system/mobile/navigation_bar_test.rb
@@ -0,0 +1,19 @@
+require "application_system_test_case"
+module Mobile
+  # TODO: Fix test when css framework has been added
+  # class NavigationBarTest < MobileSystemTestCase
+  #   test "can access sign up page via burger menu" do
+  #     visit root_path
+  #     find(".navbar-burger").click
+  #     click_on I18n.t("shared.navbar.sign_up")
+  #     assert_current_path sign_up_path
+  #   end
+
+  #   test "can access login page via burger menu" do
+  #     visit root_path
+  #     find(".navbar-burger").click
+  #     click_on I18n.t("shared.navbar.login")
+  #     assert_current_path login_path
+  #   end
+  # end
+end
